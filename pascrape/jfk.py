f9ab655b7524d969535d6a46233af9b4fa52ff9d
Garry Wright <garrywright@Garrys-MacBook-Pro.local>
2019-03-19 14:55:13 -0400

    python

---
+++ pascrape/jfk.py
@@ -0,0 +1,34 @@
+
+from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium import webdriver
+import json
+import datetime
+
+
+data = {}
+url = 'https://www.jfkairport.com/'
+try:
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+    browser = webdriver.PhantomJS()
+    browser.get(url)
+    innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a stringbr
+    browser.close()
+    soup = BeautifulSoup('<!doctype html>'+innerHTML, 'html.parser')
+    weightMatrix = []
+    links = soup.find_all('div', 'terminals-lot')
+    for link in links:
+        data = {}
+        desc = link.parent.text.split("\n")[0]
+        pct = link.parent.next_sibling.next_sibling.next.next.next.text.split()[0].split("%")[0]
+        data["lot"]=desc.strip()
+        data["pct"]=int(pct)
+        data['airport']="JFK"
+        data['asof']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        weightMatrix.append(data)
+    json_data = json.dumps(weightMatrix)
+    print(json_data)
+except Exception as e:
+    browser.close()
+
---
+++ pascrape/jfktaxi.py
@@ -0,0 +1,38 @@
+
+from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium import webdriver
+import json
+import datetime
+
+
+data = {}
+url = 'https://www.jfkairport.com/'
+try:
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+    browser = webdriver.PhantomJS()
+    browser.get(url)
+    innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a stringbr
+    browser.close()
+    soup = BeautifulSoup('<!doctype html>'+innerHTML, 'html.parser')
+    weightMatrix = []
+
+    links = soup.find_all('div', 'term-desc')
+    for link in links:
+        if len(link['class']) == 1 and not(link.find('small') is None):
+            data = {}
+            desc = link.next.next.next_sibling.next_sibling.next.next.split("\n")[0]
+            wait = link.find('span').next
+            units = link.find('small').next
+            data["terminal"]=desc.strip()
+            data["wait"]=int(wait)
+            data["units"] = units
+            data['airport']="JFK"
+            data['asof']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            weightMatrix.append(data)
+
+    json_data = json.dumps(weightMatrix)
+    print(json_data)
+except Exception as e:
+    browser.close()
